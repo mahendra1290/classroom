a85d96dbae2ea77a92f9e48e0a29cc13a8dcce7f
piyushbhutani1999 <piyushbhutaniynr@gmail.com>
2019-08-04 15:32:44 +0530

    last

--- customuser/views.py
+++ customuser/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404,HttpResponse
 from django.contrib.auth import authenticate, login,logout
 from django.contrib import messages
-from .forms import LoginForm,UserAdminChangeForm, UserAdminCreationForm
+from .forms import LoginForm,UserAdminChangeForm, UserAdminCreationForm,UserRegisterForm
 
 from .models import User
 from django.views.generic import TemplateView
