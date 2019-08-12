3bfbc52d6b3c0ea7b2d85d845e4e743f6832f0b4
mahendra1290 <mahendra.suthar.1290@gmail.com>
2019-08-12 15:41:17 +0530

    view refracted method for creating teacher and student user created

--- customuser/views.py
+++ customuser/views.py
@@ -20,26 +20,18 @@ def homepageview(request):
         if request.method == 'POST':
             form = TeacherRegistrationForm(request.POST)
             if form.is_valid():
-                password = form.cleaned_data['password']
                 email = form.cleaned_data['email']
-                name = form.cleaned_data['name']
-                phone = form.cleaned_data['phone']
-                department = form.cleaned_data['department']
+                password = form.cleaned_data['password']
                 user_list = User.objects.filter(email=email)
                 if user_list.count() is 0:
-                    user = User.objects.create_user(
+                    teacherobj = form.save(commit=False)
+                    user = User.objects.create_user_for_teacher(
                         email=email, password=password)
-                    user.is_teacher = True
-                    user.is_active = True
-                    group = Group.objects.get(name='teacher')
-                    user.groups.add(group)
-                    user.save()
-                    teacherobj = Teacher(
-                        name=name, department=department, phone=phone, user=user)
+                    teacherobj.user = user
                     teacherobj.save()
                     messages.success(
                         request, "Successfully created. Login to give assignments")
-                    return redirect('customuser:homepage')
+                    return redirect('customuser:login')
 
                 else:
                     messages.error(
@@ -50,7 +42,7 @@ def homepageview(request):
             form = TeacherRegistrationForm()
         return render(request, 'home.html', {'form': form})
     else:
-        if request.user.is_teacher is True:
+        if request.user.is_teacher:
             return redirect('teacher:homepage')
         else:
             return redirect('student:homepage')
@@ -65,10 +57,10 @@ def login_view(request):
             user_obj = authenticate(username=username, password=password)
             if user_obj is not None:
                 login(request, user_obj)
-                if request.user.is_teacher is False:
-                    return redirect('student:registration')
-                else:
+                if request.user.is_teacher:
                     return redirect('teacher:homepage')
+                else:
+                    return redirect('student:registration')
             else:
                 messages.error(request, 'Incorrect Username or Password')
     else:
@@ -78,27 +70,19 @@ def login_view(request):
 
 def signup_view(request):
     if request.method == 'POST':
-        print("signed up")
         form = UserRegisterForm(request.POST)
-        try:
-            form_obj = User.objects.get(email = request.POST['email'])
-        except:
-            form_obj = None
-        if form_obj is None:
-            if form.is_valid():
-                password = form.cleaned_data['password']
-                email = form.cleaned_data['email']
-                user = User.objects.create_user(email=email, password=password)
-                group = Group.objects.get(name='student')
-                user.groups.add(group)
-                user.is_active = True
-                user.save()
-                messages.success(request, "Successfully registered. Click on login and fill details.")
-                return redirect('customuser:homepage')
+        if form.is_valid():
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            user = User.objects.create_user_for_student(email=email, password=password)
+            messages.success(request, "Successfully registered. Click on login and fill details.")
+            return redirect('customuser:login')
+        else:
+            user = User.objects.filter(email=request.POST['email'])
+            if user.count() > 0:
+                messages.error(request, "This email address is already registered")
             else:
                 messages.error(request, "Incorrect details.")
-        else:
-            messages.error(request, "This email address is already registered. Click on login button.")
     else:
         form = UserRegisterForm()
     return render(request, 'signup.html', {'form': form})
