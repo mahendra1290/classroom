2a181eeaa87527ee1c3f5f8b6bf982ff9de06580
mahendra1290 <mahendra.suthar.1290@gmail.com>
2019-08-15 23:36:57 +0530

    teacher views models refracted

--- teacher/urls.py
+++ teacher/urls.py
@@ -4,7 +4,7 @@ from django.urls import include
 from django.contrib.auth import authenticate, login,logout
 from django.conf.urls import url
 from .views import home_page_view
-from .views import ClassroomCreateView
+from .views import classroom_create_view
 from .views import classroom_detail_view
 from .views import teacher_edit_view
 from .views import classroom_delete_view
@@ -16,7 +16,7 @@ app_name="teacher"
 urlpatterns = [
     path('', home_page_view, name='homepage'),
     path('edit/profile/',teacher_edit_view,name = 'teachers_edit' ),
-    path('classroom/add/', ClassroomCreateView.as_view(), name='create_classroom'),
+    path('classroom/add/', classroom_create_view, name='create_classroom'),
     path('classroom-<int:pk>/', classroom_detail_view, name='classroom_detail'),
     path('classroom-<int:pk>/delete/', classroom_delete_view, name='classroom_delete'),
     path('classroom-<int:pk>/edit/', classroom_edit_view, name='classroom_edit'),
