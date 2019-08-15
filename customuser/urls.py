a5a86d78749a499b2cf9fc9a6812f152b651f6c4
mahendra1290 <mahendra.suthar.1290@gmail.com>
2019-08-15 16:25:10 +0530

    404 page added

--- customuser/urls.py
+++ customuser/urls.py
@@ -9,11 +9,13 @@ from .views import login_view
 from .views import signup_view
 from .views import logout_view
 from .views import delete_user
+from .views import permission_denied_view
 
 app_name="customuser"
 
 urlpatterns = [
     path('', homepageview, name= 'homepage'),
+    path('404/', permission_denied_view, name = 'permission_denied'),
     path('login/', login_view, name = 'login'),
     path('logout/', logout_view, name = 'logout'),
     path('signup/', signup_view, name = 'signup'),
