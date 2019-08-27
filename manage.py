a37c063f527c4163d86874afe88f83c23dbbecfd
mahendra1290 <mahendra.suthar.1290@gmail.com>
2019-08-27 23:39:22 +0530

    merge error removed

--- manage.py
+++ manage.py
@@ -1,17 +1,8 @@
 #!/usr/bin/env python
-<<<<<<< HEAD
 import os
 import sys
-
-if __name__ == '__main__':
-=======
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
-
 
 def main():
->>>>>>> d6e74ed4ecdbabd00948f6ec438ca17a54a19a1a
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'classroom.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -22,10 +13,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-<<<<<<< HEAD
-=======
-
 
 if __name__ == '__main__':
     main()
->>>>>>> d6e74ed4ecdbabd00948f6ec438ca17a54a19a1a
+
