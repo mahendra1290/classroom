8b623a4fb41f942bafcea620eecaa9217923f302
piyushbhutani1999 <piyushbhutaniynr@gmail.com>
2019-08-28 04:32:51 +0530

    student password change form

--- student/views.py
+++ student/views.py
@@ -6,7 +6,8 @@ from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.exceptions import PermissionDenied
 from django.urls import reverse
-
+from django.contrib.auth import login
+from django.contrib.auth import logout
 
 from teacher.models import TeachersClassRoom
 from assignment.models import Assignment
@@ -14,7 +15,7 @@ from .models import Student
 from .forms import StudentRegistrationForm
 from .forms import JoinClassForm
 from .forms import SolutionCreateForm
-
+from customuser.forms import UserPasswordEditForm
 
 def user_is_student_check(user):
     if user.is_authenticated and user.is_student:
@@ -96,35 +97,6 @@ def join_class_view(request):
 
 
 @user_passes_test(user_is_student_registered, login_url='customuser:permission_denied')
-def student_edit_view(request):
-    student = Student.objects.get(user=request.user)
-    if request.method == 'POST':
-        form = StudentRegistrationForm(request.POST)
-        if student.can_get_rollnumber(request.POST['rollno']):
-            if form.is_valid():
-                student.name = form.cleaned_data['name']
-                student.year = form.cleaned_data['year']
-                student.branch = form.cleaned_data['branch']
-                student.rollno = form.cleaned_data['rollno']
-                user = request.user
-                student.save()
-                messages.success(request, "Profile updated successfully")
-                return redirect('student:homepage')
-            
-            else:
-                messages.error(request, "Incorrect Details")
-        else:
-            messages.error(request, "Roll number is already registered")
-    else:
-        form = StudentRegistrationForm(initial={
-                                       'name': student.name,
-                                       'year': student.year,
-                                       'branch': student.branch,
-                                       'rollno': student.rollno})
-    return render(request, 'student_edit_view.html', {'form': form})
-
-
-@user_passes_test(user_is_student_registered, login_url='customuser:permission_denied')
 def classroom_detail_view(request, slug):
     try:
         classroom = TeachersClassRoom.objects.get(slug=slug)
@@ -151,3 +123,64 @@ def classroom_exit_view(request, slug):
     student.save()
     messages.success(request, "Successfully exit the classroom")
     return redirect('student:homepage')
+
+@user_passes_test(user_is_student_registered, login_url='customuser:permission_denied')
+def student_edit_view(request):
+    student = Student.objects.get(user=request.user)
+    password_change_form = UserPasswordEditForm()
+    student_edit_form = StudentRegistrationForm
+    if request.method == 'POST':
+        if 'studentedit_submit' in request.POST:
+            student_edit_form = StudentRegistrationForm(request.POST)
+            if student.can_get_rollnumber(request.POST['rollno']):
+                if student_edit_form.is_valid():
+                    student.name = student_edit_form.cleaned_data['name']
+                    student.year = student_edit_form.cleaned_data['year']
+                    student.branch = student_edit_form.cleaned_data['branch']
+                    student.rollno = student_edit_form.cleaned_data['rollno']
+                    user = request.user
+                    student.save()
+                    messages.success(request, "Profile updated successfully")
+                    return redirect('student:homepage')
+            
+                else:
+                    messages.error(request, "Incorrect Details")
+            else:
+                messages.error(request, "Roll number is already registered")
+
+        else:
+            student_edit_form = StudentRegistrationForm(instance=student)
+            password_change_form = UserPasswordEditForm(request.POST)
+            if password_change_form.is_valid():
+                user = request.user
+                old_password = password_change_form.cleaned_data['old_password']
+                new_password = password_change_form.cleaned_data['new_password']
+                confirm_password = password_change_form.cleaned_data['confirm_password']
+                if user.check_password(old_password) and new_password == confirm_password:
+                    user.set_password(new_password)
+                    user.save()
+                    user = authenticate(
+                        request, username=user.email, password=new_password)
+                    if user is not None:
+                        login(request, user)
+                        messages.success(
+                            request, 'Password is updated successfully')
+                        return redirect('student:homepage')
+                elif new_password != confirm_password:
+                    messages.error(
+                        request, 'New Password and Confirm Password must be same')
+                else:
+                    messages.error(request, 'Current Password is not correct.')
+                return render(request, 'student_edit_view.html', {
+                    'passwordEditForm': password_change_form,
+                    'studentEditForm': student_edit_form,
+                    'student': student,
+                })
+    else:
+        student_edit_form = StudentRegistrationForm(instance=student)
+        password_change_form = UserPasswordEditForm()
+    return render(request, 'student_edit_view.html', {
+        'studentEditForm': student_edit_form,
+        'passwordEditForm': password_change_form,
+        'student': student
+    })
